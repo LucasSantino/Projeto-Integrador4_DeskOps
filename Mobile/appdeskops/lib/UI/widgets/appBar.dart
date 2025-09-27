a29157078b01300b18cf1aa12cbb256867bc8796
LucasSantino <lsantino541@gmail.com>
2025-09-27 17:16:37 -0300

    Update appBar.dart

--- Mobile/appdeskops/lib/UI/widgets/appBar.dart
+++ Mobile/appdeskops/lib/UI/widgets/appBar.dart
@@ -4,11 +4,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final VoidCallback? onDrawerTap;
   final VoidCallback? onNotificationTap;
 
-  const CustomAppBar({
-    super.key,
-    this.onDrawerTap,
-    this.onNotificationTap,
-  });
+  const CustomAppBar({super.key, this.onDrawerTap, this.onNotificationTap});
 
   @override
   Widget build(BuildContext context) {
@@ -16,28 +12,40 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       color: Colors.black,
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // Ícone de notificações à esquerda
-            IconButton(
-              icon: const Icon(Icons.notifications, color: Colors.white),
-              onPressed: onNotificationTap ?? () {},
-            ),
+        child: SizedBox(
+          height: preferredSize.height,
+          child: Row(
+            children: [
+              // Ícone do drawer à esquerda
+              IconButton(
+                icon: const Icon(Icons.menu, color: Colors.white),
+                onPressed: onDrawerTap ?? () {},
+              ),
 
-            // Logo central
-            Image.asset(
-              'assets/images/logodeskops.png',
-              width: 120,
-              fit: BoxFit.contain,
-            ),
+              // Espaço flexível para a logo central
+              Expanded(
+                child: Center(
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Image.asset(
+                        'assets/images/logodeskops.png',
+                        width:
+                            constraints.maxWidth *
+                            0.99, // ocupação do espaço disponível
+                        fit: BoxFit.contain,
+                      );
+                    },
+                  ),
+                ),
+              ),
 
-            // Ícone do drawer à direita
-            IconButton(
-              icon: const Icon(Icons.menu, color: Colors.white),
-              onPressed: onDrawerTap ?? () {},
-            ),
-          ],
+              // Ícone de notificações à direita
+              IconButton(
+                icon: const Icon(Icons.notifications, color: Colors.white),
+                onPressed: onNotificationTap ?? () {},
+              ),
+            ],
+          ),
         ),
       ),
     );
