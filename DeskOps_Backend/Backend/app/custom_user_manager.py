a4786a7f3324ebfdbdbef48aaee4c106f622e688
Pedro <pguerra872@gmail.com>
2025-10-02 22:42:45 -0300

    up
    02.10

---
+++ DeskOps_Backend/Backend/app/custom_user_manager.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, name, cpf, cargo, dt_nascimento, 
+                       pais, estado, cidade, rua, bairro, cep, numero, **extra_fields):
+        
+        if None in (email, password, name, cpf,cargo, dt_nascimento, 
+                       pais, estado, cidade, rua, bairro, cep, numero):
+            raise ValueError('Missing required fields during user creation')
+        
+        extra_fields.setdefault('is_active',True)
+        user = self.model(email=self.normalize_email(email), 
+                          name=name, 
+                          cpf=cpf, 
+                          cargo=cargo, 
+                          dt_nascimento=dt_nascimento, 
+                          pais=pais, 
+                          estado=estado, 
+                          cidade=cidade, 
+                          rua=rua, 
+                          bairro=bairro, 
+                          cep=cep, 
+                          numero=numero, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password, name, cpf, cargo, dt_nascimento, 
+                       pais, estado, cidade, rua, bairro, cep, numero, **extra_fields):
+        
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+
+        return self.create_user(self, email, password, name, cpf, cargo, dt_nascimento, 
+                       pais, estado, cidade, rua, bairro, cep, numero, **extra_fields)
