26d5f6244cce41e0ce16cf9f10df396863ea87e4
Victor Ribeiro <152220391+RSVictor@users.noreply.github.com>
2025-12-11 00:58:06 -0300

    Update api.js

--- DeskOps_Web/deskops/src/services/api.js
+++ DeskOps_Web/deskops/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://deskops-v1-cyfhfecpc0decmd8.westus3-01.azurewebsites.net/api",
+  baseURL: "https://deskops-v1-cyfhfecpc0decmd8.westus3-01.azurewebsites.net/api/",
   headers: {
     "Content-Type": "application/json",
   },
